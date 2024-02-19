66d00108d41514078d29239269d5e26e2938e3ac
“HUIXIN <2470901z@student.gla.ac.uk>
2024-02-19 15:33:41 +0000

    instruction

--- interpreter.js
+++ interpreter.js
@@ -100,28 +100,29 @@ class Order {
 
 class Tape {
   static codeTable = {
-    '1': 0b10000,
-    '2': 0b01000,
-    '3': 0b11001,
-    '4': 0b00100,
-    '5': 0b10101,
-    '6': 0b01101,
-    '7': 0b11100,
-    '8': 0b00010,
-    '9': 0b10011,
-    '0': 0b00001,
-    '+': 0b01011,
-    '-': 0b11010,
-    '.': 0b00011,
-    '=': 0b01010,
-    'n': 0b11000,
-    'v': 0b00110,
-    ' ': 0b01110,
+    '1': 0b00001,    // 1
+    '2': 0b00010,    // 2
+    '3': 0b10011,    // 25
+    '4': 0b00100,    // 4
+    '5': 0b10101,    // 21
+    '6': 0b10110,    // 22
+    '7': 0b00111,    // 7
+    '8': 0b01000,    // 8
+    '9': 0b11001,    // 25
+    '0': 0b10000,    // 15
+    '+': 0b11010,    // 26
+    '-': 0b01011,    // 11
+    '.': 0b11100,    // 28
+    '=': 0b01010,    // 10
+    'n': 0b00011,    // 3
+    'v': 0b01100,    // 12
+    ' ': 0b01110,    // 13
 
-    '\n': 0b10110,
-    '\r': 0b01111,
+    '\n': 0b01101,   // 13
+    '\r': 0b11110,   // 30
 
-    '#': 0b11011 // We represent LS with a hash because its connection to modern-day comments
+    // We represent LS with a hash because its connection to modern-day comments
+    '#': 0b11011     // 27
   };
 
   constructor(text) {
@@ -163,109 +164,112 @@ class Tape {
     if (this.location >= this.content.length) {
       return 0;
     } else {
-      return this.content[this.location++];
+      let character = this.content[this.location];
+      console.log(`Reading ${character} from tape`);
+      this.location++;
+      return character;
     }
   }
 }
 
 class SOLIDAC {
   static initialOrderStore = [
-    this.wordFromOrder(55, 0, 10),    //  0  d = 10
-    this.wordFromOrder(40, 0, 100),   //  1  s[100] = m; m = l = 0
-    this.wordFromOrder( 5, 4, 2045),  //  2  b[4] = 2045
-    this.wordFromOrder( 5, 2, 0),     //  3  b[2] = 0
-    this.wordFromOrder( 5, 5, 0),     //  4  b[5] = 0
-    this.wordFromOrder( 5, 3, 0),     //  5  b[3] = 0
-    this.wordFromOrder( 6, 4, 2),     //  6  b[4] += 2
-    this.wordFromOrder(40, 0, 98),    //  7  s[98] = m; m = l = 0
-    this.wordFromOrder(31, 0, 97),    //  8  s[97] = l
-    this.wordFromOrder(10, 1, 96),    //  9  s[96] = b[1] = input
-    this.wordFromOrder(32, 0, 86),    // 10  l = s[86]
-    this.wordFromOrder(43, 0, 37),    // 11  m += s[37]
-    this.wordFromOrder(38, 1, 0),     // 12  a <<= b[1]
-    this.wordFromOrder(22, 0, 54),    // 13  if (a > 0) goto 54
-    this.wordFromOrder(56, 0 ,97),    // 14  a = d * s[97]
-    this.wordFromOrder( 8, 1, 30),    // 15  b[1] &= s[30]
-    this.wordFromOrder( 1, 1, 96),    // 16  s[96] = b[1]
-    this.wordFromOrder(33, 0, 96),    // 17  a += s[96]
-    this.wordFromOrder( 6, 3, 1),     // 18  b[3] += 1
-    this.wordFromOrder(26, 0, 8),     // 19  goto 8
-    this.wordFromOrder(14, 1, 57),    // 20  b[1] -= 1; if (b[1] != 0) goto 57
-    this.wordFromOrder(13, 5, 40),    // 21  if (b[5] != 0) goto 40
-    this.wordFromOrder(13, 3, 40),    // 22  if (b[3] != 0) goto 40
-    this.wordFromOrder(13, 1, 9),     // 23  if (b[1] != 0) goto 9
-    this.wordFromOrder(20, 0, 96),    // 24  output s[96]
-    this.wordFromOrder( 6, 3, 14),    // 25  b[3] += 14
-    this.wordFromOrder(13, 3, 28),    // 26  if (b[3] != 0) goto 28
-    this.wordFromOrder(13, 1, 9),     // 27  if (b[1] != 0) goto 9
-    this.wordFromOrder(10, 3, 96),    // 28  s[96] = b[3] = input
-    this.wordFromOrder(13, 3, 31),    // 29  if (b[3] != 0) goto 31
-    this.wordFromOrder( 5, 1, 15),    // 30  b[1] = 15
-    this.wordFromOrder( 7, 3, 27),    // 31  b[3] -= 27
-    this.wordFromOrder(13, 3, 24),    // 32  if (b[3] != 0) goto 24
-    this.wordFromOrder( 5, 1, 0),     // 33  b[1] = 0
-    this.wordFromOrder(26, 0, 24),    // 34  goto 24
-    this.wordFromOrder(14, 1, 48),    // 35  b[1] -= 1; if (b[1] != 0) goto 48
-    this.wordFromOrder(13, 3, 38),    // 36  if (b[3] != 0) goto 38
-    this.wordFromOrder(26, 3, 9),     // 37  goto b[3] + 9
-    this.wordFromOrder(13, 2, 46),    // 38  if (b[2] != 0) goto 46
-    this.wordFromOrder(17, 0, 99),    // 39  add s[99] to next order
-    this.wordFromOrder( 0, 0, 0),     // 40  execute s[99] or stop
-    this.wordFromOrder( 5, 1, 1),     // 41  b[1] = 1
-    this.wordFromOrder( 3, 1, 99),    // 42  b[1] += s[99]
-    this.wordFromOrder( 1, 1, 99),    // 43  s[99] = b[1]
-    this.wordFromOrder( 1, 1, 100),   // 44  s[100] = b[1]
-    this.wordFromOrder(16, 0, 2046),  // 45  add 2046 to next order's address part
-    this.wordFromOrder(40, 2, 100),   // 46  n = 98 or 100; s[(b[2] + n) % 2048] = m; m = l = 0
-    this.wordFromOrder(26, 0, 2),     // 47  goto 2
-    this.wordFromOrder(14, 1, 74),    // 48  b[1] -= 1; if (b[1] != 0) goto 74
-    this.wordFromOrder(26, 0, 9),     // 49  goto 9
-    this.wordFromOrder( 5, 1, 1024),  // 50  b[1] = 1024
-    this.wordFromOrder(40, 1, 2047),  // 51  s[b[1] - 1] = m; m = l = 0
-    this.wordFromOrder(14, 1, 51),    // 52  b[1] -= 1; if (b[1] != 0) goto 51
-    this.wordFromOrder(26, 0, 0),     // 53  goto 0
-    this.wordFromOrder( 7, 1, 26),    // 54  b[1] -= 26
-    this.wordFromOrder(13, 1, 20),    // 55  if (b[1] != 0) goto 20
-    this.wordFromOrder(26, 0, 83),    // 56  goto 83
-    this.wordFromOrder(42, 0, 97),    // 57  m = s[97]
-    this.wordFromOrder(12, 3, 61),    // 58  if (b[3] > 0) goto 61
-    this.wordFromOrder(17, 0, 97),    // 59  add s[97] to next order
-    this.wordFromOrder(42, 0, 100),   // 60  m = s[s[97] + 100]
-    this.wordFromOrder(13, 4, 64),    // 61  if (b[4] != 0) goto 64
-    this.wordFromOrder(40, 0, 97),    // 62  s[97] = m; m = l = 0
-    this.wordFromOrder(44, 0, 97),    // 63  m -= s[97]
-    this.wordFromOrder(14, 1, 69),    // 64  b[1] -= 1; if (b[1] != 0) goto 69
-    this.wordFromOrder(38, 5, 14),    // 65  a <<= (b[5] + 14) % 2048
-    this.wordFromOrder( 5, 5, 2045),  // 66  b[5] = 2045
-    this.wordFromOrder(43, 0, 98),    // 67  m += s[98]
-    this.wordFromOrder(26, 0, 5),     // 68  goto 5
-    this.wordFromOrder(43, 0, 98),    // 69  m += s[98]
-    this.wordFromOrder(14, 1, 35),    // 70  b[1] -= 1; if (b[1] != 0) goto 35
-    this.wordFromOrder( 5, 4, 2046),  // 71  b[4] = 2046
-    this.wordFromOrder( 5, 3, 1024),  // 72  b[3] = 1024
-    this.wordFromOrder(26, 0, 6),     // 73  goto 6
-    this.wordFromOrder( 6, 1, 21),    // 74  b[1] += 21
-    this.wordFromOrder(13, 1, 81),    // 75  if (b[1] != 0) goto 81
-    this.wordFromOrder(13, 2, 90),    // 76  if (b[2] != 0) goto 90
-    this.wordFromOrder(40, 0, 99),    // 77  s[99] = m; m = l = 0
-    this.wordFromOrder( 2, 1, 99),    // 78  b[1] = s[99]
-    this.wordFromOrder( 1, 1, 100),   // 79  s[100] = b[1]
-    this.wordFromOrder(26, 0, 4),     // 80  goto 4
-    this.wordFromOrder(14, 1, 85),    // 81  b[1] -= 1; if (b[1] != 0) goto 85
-    this.wordFromOrder( 5, 4, 2046),  // 82  b[4] = 2046
-    this.wordFromOrder(13, 3, 40),    // 83  if (b[3] != 0) goto 40
-    this.wordFromOrder(26, 0, 6),     // 84  goto 6
-    this.wordFromOrder(14, 1, 94),    // 85  b[1] -= 1; if (b[1] != 0) goto 94
-    this.wordFromOrder(12, 4, 72),    // 86  if (b[4] > 0) goto 72
-    this.wordFromOrder(13, 3, 72),    // 87  if (b[3] != 0) goto 72
-    this.wordFromOrder( 5, 2, 2046),  // 88  b[2] = 2046
-    this.wordFromOrder(26, 0, 4),     // 89  goto 4
-    this.wordFromOrder(21, 0, 94),    // 90  if (a < 0) goto 94
-    this.wordFromOrder(40, 0, 98),    // 91  s[98] = m; m = l = 0
-    this.wordFromOrder( 2, 2, 98),    // 92  b[2] = s[98]
-    this.wordFromOrder(13, 2, 4),     // 93  if (b[2] != 0) goto 4
-    this.wordFromOrder(15, 1, 21),    // 94  b[1] -= 2; if (b[1] != 0) goto 21
-    this.wordFromOrder(26, 0, 36)     // 95  goto 36
+    this.wordFromOrder(55, 0, 10),    // 0
+    this.wordFromOrder( 5, 2, 0),
+    this.wordFromOrder( 5, 3, 15),
+    this.wordFromOrder( 5, 4, 0),
+    this.wordFromOrder(40, 0, 98),
+    this.wordFromOrder(31, 0, 97),
+    this.wordFromOrder(10, 1, 96),
+    this.wordFromOrder(32, 0, 35),
+    this.wordFromOrder(43, 0, 64),
+    this.wordFromOrder(38, 1, 0),
+    this.wordFromOrder(22, 0, 31),    // 10
+    this.wordFromOrder(56, 0 ,97),
+    this.wordFromOrder( 8, 1, 2),
+    this.wordFromOrder( 1, 1, 96),
+    this.wordFromOrder(33, 0, 96),
+    this.wordFromOrder( 6, 4, 1),
+    this.wordFromOrder(26, 0, 5),
+    this.wordFromOrder(12, 1, 6),
+    this.wordFromOrder( 6, 1, 20),
+    this.wordFromOrder(13, 1, 22),
+    this.wordFromOrder(41, 0, 99),    // 20
+    this.wordFromOrder(26, 0, 47),
+    this.wordFromOrder(14, 1, 26),
+    this.wordFromOrder( 5, 3, 0),
+    this.wordFromOrder(13, 4, 42),
+    this.wordFromOrder(26, 0, 4),
+    this.wordFromOrder(14, 1, 28),
+    this.wordFromOrder(26, 0, 91),
+    this.wordFromOrder(15, 1, 32),
+    this.wordFromOrder(13, 4, 40),
+    this.wordFromOrder(26, 0, 6),     // 30
+    this.wordFromOrder(13, 1, 54),
+    this.wordFromOrder(13, 2, 42),
+    this.wordFromOrder( 6, 1, 8),
+    this.wordFromOrder(13, 1, 29),
+    this.wordFromOrder(12, 4, 37),
+    this.wordFromOrder( 0, 0, 0),
+    this.wordFromOrder( 2, 1, 97),
+    this.wordFromOrder(12, 1, 46),
+    this.wordFromOrder( 0, 0, 0),
+    this.wordFromOrder(13, 1, 42),    // 40
+    this.wordFromOrder(17, 0, 99),
+    this.wordFromOrder( 0, 0, 0),
+    this.wordFromOrder(42, 0, 11),
+    this.wordFromOrder(39, 0, 19),
+    this.wordFromOrder(45, 0, 99),
+    this.wordFromOrder(42, 0, 99),
+    this.wordFromOrder(48, 0, 95),
+    this.wordFromOrder(40, 1, 100),
+    this.wordFromOrder(26, 0, 1),
+    this.wordFromOrder( 5, 1, 1024),  // 50
+    this.wordFromOrder(40, 1, 2047),
+    this.wordFromOrder(14, 1, 51),
+    this.wordFromOrder(26, 0, 0),
+    this.wordFromOrder(42, 0, 97),
+    this.wordFromOrder(12, 4, 58),
+    this.wordFromOrder(17, 0, 97),
+    this.wordFromOrder(42, 0, 100),
+    this.wordFromOrder(13, 3, 61),
+    this.wordFromOrder(40, 0, 97),
+    this.wordFromOrder(44, 0, 97),    // 60
+    this.wordFromOrder( 7, 1, 26),
+    this.wordFromOrder(13, 1, 65),
+    this.wordFromOrder( 5, 3, 15),
+    this.wordFromOrder(26, 3, 9),
+    this.wordFromOrder(14, 1, 81),
+    this.wordFromOrder(13, 4, 42),
+    this.wordFromOrder(13, 2, 42),
+    this.wordFromOrder(20, 0, 96),
+    this.wordFromOrder( 6, 1, 18),
+    this.wordFromOrder(13, 1, 72),    // 70
+    this.wordFromOrder(12, 4, 6),
+    this.wordFromOrder(10, 1, 96),
+    this.wordFromOrder(13, 1, 75),
+    this.wordFromOrder( 5, 4, 0),
+    this.wordFromOrder( 7, 1, 27),
+    this.wordFromOrder(13, 1, 78),
+    this.wordFromOrder( 5, 4, 1024),
+    this.wordFromOrder( 7, 1, 4),
+    this.wordFromOrder(12, 1, 72),
+    this.wordFromOrder(26, 0, 68),    // 80
+    this.wordFromOrder(14, 1, 88),
+    this.wordFromOrder(12, 4, 84),
+    this.wordFromOrder( 0, 0, 0),
+    this.wordFromOrder(38, 2, 14),
+    this.wordFromOrder( 5, 2, 2045),
+    this.wordFromOrder(43, 0, 98),
+    this.wordFromOrder(26, 0, 2),
+    this.wordFromOrder(43, 0, 98),
+    this.wordFromOrder(14, 1, 93),
+    this.wordFromOrder( 5, 3, 0),     // 90
+    this.wordFromOrder( 5, 4, 1024),
+    this.wordFromOrder(26, 0, 4),
+    this.wordFromOrder(14, 1, 17),
+    this.wordFromOrder(26, 0, 29),
+    this.wordFromOrder( 0, 0, 2047)
   ];
 
   static stopReasons = {
@@ -299,7 +303,7 @@ class SOLIDAC {
     this.registers = {
       b: Array.from({length: 8}, () => new MachineWord(11, false)), //B-Registers   
   
-      m: new MachineWord(21, false), //High half of double-length accumulator
+      m: new MachineWord(21), //High half of double-length accumulator
       l: new MachineWord(19, false, false), //Low half of double-length accumulator
       d: new MachineWord(21), //The multiplier/quotient register      
       s: new MachineWord(21), //The store-transfer register
@@ -317,16 +321,19 @@ class SOLIDAC {
 
     this.optionalStopEnabled = false;
     this.slowMode = false;
-    this.singleShot = false;
-    this.tape = [];
+    this.tape = null;
 
     this.outputHook = function () {};
     this.postOrderHook = function () {};
     this.stoppedHook = function () {};
+    
+    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    this.audioNodes = {};
   }
 
   initialSet() {
     this.counter.clear();
+    this.currentModifier = null;
     this.registers.m.clear();
     this.registers.l.clear();
     this.registers.d.clear();
@@ -386,15 +393,20 @@ class SOLIDAC {
         this.registers.b[b].subtract(n);
         break;
       case 8:
-        this.registers.b[b].and({number: n, size: 11});
+        this.registers.b[b].and(this.readStore(n));
         break;
       case 9:
         this.writeStore(n, this.registers.b[b], 'swap');
         break;
       case 10:
-        const tapeInput = this.tape.read();
-        this.registers.b[b].assign({number: tapeInput, size: 11});
-        this.writeStore(n, {number: tapeInput, size: 20});
+        if (this.tape == null) {
+          this.stopReason = this.constructor.stopReasons.absolute;
+          alert('Trying to read tape but no tape is given.');
+        } else {
+          const tapeInput = this.tape.read();
+          this.registers.b[b].assign({number: tapeInput, size: 11});
+          this.writeStore(n, {number: tapeInput, size: 20});
+        }
         break;
       case 12:
         if (this.registers.b[b].toNumber() > 0) {
@@ -485,10 +497,28 @@ class SOLIDAC {
         else this.currentReadingStore = this.mainStore;
         break;
       case 28:
-        // TODO Figure how wire input/output works
+        // We use wire output for audio
+        if (n % 2 === 0) {
+          let oscillator;
+
+          if (n in this.audioNodes) {
+            oscillator = this.audioNodes[n];
+          } else {
+            oscillator = this.audioContext.createOscillator();
+            oscillator.type = "triangle";
+            this.audioNodes[n] = oscillator;
+            oscillator.start();
+          }
+
+          oscillator.frequency.setValueAtTime(this.registers.b[b].toNumber(), this.audioContext.currentTime);
+          oscillator.connect(this.audioContext.destination);
+        }
         break;
       case 29:
-        // TODO Figure how wire output works
+        if (n % 2 === 0 && n in this.audioNodes) {
+          let oscillator = this.audioNodes[n];
+          oscillator.disconnect(this.audioContext.destination);
+        }
         break;
       case 31:
         n = this.applyModifier(b, n);
@@ -556,12 +586,10 @@ class SOLIDAC {
         this.writeStore(n, this.registers.m);
         this.registers.m.clear();
         this.registers.l.clear();
-        if (this.registers.s.hasOverflowed()) this.stopReason = this.constructor.stopReasons.irrecoverableOverflow;
         break;
       case 41:
         n = this.applyModifier(b, n);
         this.writeStore(n, this.registers.m);
-        if (this.registers.s.hasOverflowed()) this.stopReason = this.constructor.stopReasons.irrecoverableOverflow;
         break;
       case 42:
         n = this.applyModifier(b, n);
@@ -578,20 +606,15 @@ class SOLIDAC {
         break;
       case 45:
         n = this.applyModifier(b, n);
-        var s = this.currentReadingStore[n];
-        this.registers.s.assign(this.registers.m);
-        if (this.registers.s.hasOverflowed()) this.stopReason = this.constructor.stopReasons.irrecoverableOverflow;
-        this.registers.s.add(s);
+        this.readStore(n);
+        this.registers.s.add(this.registers.m);
         if (this.registers.s.hasOverflowed()) this.stopReason = this.constructor.stopReasons.irrecoverableOverflow;
         this.writeStore(n, this.registers.s);
         break;
       case 46:
         n = this.applyModifier(b, n);
-        var s = this.currentReadingStore[n];
-        this.registers.clear();
+        this.readStore(n);
         this.registers.s.subtract(this.registers.m);
-        if (this.registers.s.hasOverflowed()) this.stopReason = this.constructor.stopReasons.irrecoverableOverflow;
-        this.registers.s.add(s);
         if (this.registers.s.hasOverflowed()) this.stopReason = this.constructor.stopReasons.irrecoverableOverflow;
         this.writeStore(n, this.registers.s);
         break;
@@ -632,10 +655,14 @@ class SOLIDAC {
         this.registers.d.assign({number: n, size: 11});
         break;
       case 56:
-        // TODO multiplication
+        n = this.applyModifier(b, n);
+        var s = this.readStore(n);
+        var prod = s.toNumber() * this.registers.d.toNumber();
+        this.registers.m.number = Math.trunc(prod / Math.pow(2, 19)) & 0b111111111111111111111;
+        this.registers.l.number = prod & 0b1111111111111111111;
         break;
       case 58:
-        // TODO division
+        // TODO Division
         break;
       case 59:
         this.registers.m.swap(this.registers.d);
@@ -645,14 +672,14 @@ class SOLIDAC {
         this.registers.v.assign(this.readStore(n));
         break;
       case 61:
-        // TODO bind hand switches
+        // TODO Binding of hand switches
         break;
       default:
         this.stopReason = this.constructor.stopReasons.absolute;
         break;
       case 0:
         if (b == 0 && n == 0) this.stopReason = this.constructor.stopReasons.absolute;
-        else if (b == 0) this.stopReason = this.constructor.stopReasons.normal; 
+        else if (b == 0) this.stopReason = this.constructor.stopReasons.normal;
         else if (this.optionalStopEnabled) this.stopReason = this.constructor.stopReasons.normal;
         restoreStatusIndicators();
         break;
@@ -796,6 +823,13 @@ class SOLIDAC {
     
     this.registers.c.assign(this.readStore(orderLocation));
 
+    if (this.currentModifier != null) {
+      this.registers.c.number = ((this.registers.c.number >>> this.currentModifier.size) << this.currentModifier.size) |
+        interpretAsUnsigned(this.registers.c.number + this.currentModifier.number, this.currentModifier.size);
+
+      this.currentModifier = null;
+    }
+
     let f = (this.registers.c.number >>> 14) & 0b111111;
     let b = (this.registers.c.number >>> 11) & 0b111;
     let n = this.registers.c.number & 0b11111111111;
@@ -818,7 +852,9 @@ class SOLIDAC {
   }
 
   writeStore(n, v, op = 'assign') {
-    this.registers.s.clear();
+    if (!Object.is(v, this.registers.s)) {
+      this.registers.s.clear();
+    }
     this.registers.s[op](v);
     this.mainStore[n].assign(this.registers.s);
   }
